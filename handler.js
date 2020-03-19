9ca6cdd4b4a3d17acc210aaa47f8fa42b9051b5b
OxxO98 <hbok1119@naver.com>
2020-03-19 18:32:17 +0900

    kikuban설정 완료

--- handler.js
+++ handler.js
@@ -12,52 +12,10 @@ function resizeDone(){
   }
   else{
     $("#wrap").css("marginLeft", "0px");
+    $("#wrap").css("marginRight", "0px");
   }
 }
-//kikuban
-var columnWidth = 246;
-var columnGap = 12;
-var webSixColumn = (columnWidth+"px ").repeat(6);
-var webFourColumn = "375px 375px 375px 375px";
-var webThreeColumn = (columnWidth*2+columnGap+"px ").repeat(3);
-var webTwoColumn = (columnWidth*3+columnGap*2+"px ").repeat(2);
-var iPhoneTwoColumn = "375px 375px";
-var iPhoneOneColumn = "762px";
 
-webSixColumn = webSixColumn.substring(0, webSixColumn.length-1);
-webThreeColumn = webThreeColumn.substring(0, webThreeColumn.length-1);
-webTwoColumn = webTwoColumn.substring(0, webTwoColumn.length-1);
-
-if(window.matchMedia("(max-width: 1536px)").matches){
-  //iPad
-  if( $(".kikuban1 .kikuban2").css("grid-template-columns") == webSixColumn ){
-    //6단일때 3단으로 바꾸기
-    $(".kikuban1 .kikuban2").css("grid-template-columns", webThreeColumn);
-  } else if ( $(".kikuban1 .kikuban2").css("grid-template-columns") == webFourColumn ) {
-    //4단일때 2단으로 바꾸기
-    $(".kikuban1").css("grid-template-columns", webTwoColumn);
-  } else if ( $(".kikuban1").css("grid-template-columns") == webThreeColumn ) {
-    //3단일때 1단으로 바꾸기
-    $(".kikuban1").css("grid-template-columns", "1536px");
-  } else if ( $(".kikuban1").css("grid-template-columns") == webTwoColumn ) {
-    //2단일때 1단으로 바꾸기
-    $(".kikuban1").css("grid-template-columns", "1536px");
-  }
-} else if(window.matchMedia("(max-width: 768px)").matches){
-  //iPhone
-  if( $(".kikuban1 .kikuban2").css("grid-template-columns") == webSixColumn ){
-    //6단일때 2단으로 바꾸기
-    $(".kikuban1").css("grid-template-columns", iPhoneTwoColumn);
-  } else if ( $(".kikuban1 .kikuban2").css("grid-template-columns") == webFourColumn ) {
-    //4단일때 2단으로 바꾸기
-    $(".kikuban1").css("grid-template-columns", iPhoneTwoColumn);
-  } else if ( $(".kikuban1").css("grid-template-columns") == webThreeColumn ) {
-    //3단일때 1단으로 바꾸기
-    $(".kikuban1").css("grid-template-columns", iPhoneOneColumn);
-  } else if ( $(".kikuban1").css("grid-template-columns") == webTwoColumn ) {
-    //2단일때 1단으로 바꾸기
-    $(".kikuban1").css("grid-template-columns", iPhoneOneColumn);
-  }
-} else {
-  //web
-}
+matchHeader($("#header"));
+matchKikubanColumns($("#index"));
+matchKikubanColumns($("#container"));
---
+++ kikuban.js
@@ -0,0 +1,141 @@
+//kikuban
+
+//변수 선언
+var columnWidth = 246;
+var columnGap = 12;
+
+var webSixColumn = (columnWidth+"px ").repeat(6);
+var webFourColumn = "375px 375px 375px 375px";
+var webThreeColumn = (columnWidth*2+columnGap+"px ").repeat(3);
+var webTwoColumn = (columnWidth*3+columnGap*2+"px ").repeat(2);
+var iPhoneThreeColumn = "242px 242px 242px";
+var iPhoneTwoColumn = "375px 375px";
+var iPhoneOneColumn = "762px";
+
+webSixColumn = webSixColumn.substring(0, webSixColumn.length-1);
+webThreeColumn = webThreeColumn.substring(0, webThreeColumn.length-1);
+webTwoColumn = webTwoColumn.substring(0, webTwoColumn.length-1);
+
+// 이미지 수정
+function imageEdit(target, ratio){
+  console.log(target.find("img"));
+
+  var imgArr = target.find("img");
+  imgArr.each(function(){
+    var origin = $(this).width();
+    $(this).css("width", origin/ratio+"px");
+  });
+
+}
+//2xcol정리하기
+function col2xEdit(target, column){
+  target.after("<div class=\"kikubanAppend\"></div>");
+
+  target.siblings(".kikubanAppend").css("display", "grid");
+  target.siblings(".kikubanAppend").css("grid-template-columns", iPhoneTwoColumn);
+
+  var tmp1 ="", tmp2="";
+
+  for(i=0; i<column/2; i++){
+    tmp1 += "<div>";
+    tmp1 += $(".kikuban2").children("div").eq(i+column/2).html();
+    tmp1 += "</div>";
+  }
+  for(i=0; i<column/2; i++){
+    tmp2 += "<div>";
+    tmp2 += $(".kikuban2").children("div").eq(i+column/2*2).html();
+    tmp2 += "</div>";
+  }
+  for(i=0; i<column/2; i++){
+    target.children("div").eq(i+column/2).hide();
+    target.children("div").eq(i+column/2*2).hide();
+  }
+
+  target.siblings(".kikubanAppend").html(tmp1);
+}
+//3xcol정리하기
+function col3xEdit(target, column){
+  target.after("<div class=\"kikubanAppend\"></div>");
+  target.after("<div class=\"kikubanAppend\"></div>");
+
+  target.siblings(".kikubanAppend").css("display", "grid");
+  target.siblings(".kikubanAppend").css("grid-template-columns", iPhoneTwoColumn);
+
+  var tmp1 ="", tmp2="";
+
+  for(i=0; i<column/3; i++){
+    tmp1 += "<div>";
+    tmp1 += $(".kikuban2").children("div").eq(i+column/3).html();
+    tmp1 += "</div>";
+  }
+  for(i=0; i<column/3; i++){
+    tmp2 += "<div>";
+    tmp2 += $(".kikuban2").children("div").eq(i+column/3*2).html();
+    tmp2 += "</div>";
+  }
+  for(i=0; i<column/3; i++){
+    target.children("div").eq(i+column/3).hide();
+    target.children("div").eq(i+column/3*2).hide();
+  }
+
+  target.siblings(".kikubanAppend").html(tmp1);
+  target.siblings(".kikubanAppend").next().html(tmp2);
+}
+//국배판 추가
+function appendKikuban(target, column, device){
+  if(device == "iPad"){
+
+  } else if(device == "iPhone"){
+    if(column == 6){
+      target.css("grid-template-columns", iPhoneTwoColumn);
+      col3xEdit(target, 6);
+    } else if(column == 4){
+      target.css("grid-template-columns", iPhoneTwoColumn);
+      col2xEdit(target, 4);
+    } else if(column == 3){
+      target.css("grid-template-columns", iPhoneOneColumn);
+      col3xEdit(target, 3);
+    } else if(column == 2){
+      target.css("grid-template-columns", iPhoneTwoColumn);
+    } else {
+      target.css("grid-template-columns", iPhoneOneColumn);
+      imageEdit(target, 2);
+    }
+  }
+}
+//칼럼 매칭
+function matchKikubanColumns (target){
+  if( $(target).children(".kikuban1").length){
+    target = target.children(".kikuban1");
+  } else {
+    target = target.children(".kikuban2");
+  }
+
+  var col = target.css("--col");
+  console.log(target.css("--col"));
+
+  if (window.matchMedia("(max-device-width: 1536px)").matches && window.matchMedia("(max-device-width: 768px)").matches == false){
+    //iPad
+    appendKikuban(target, col, "iPad");
+  } else if(window.matchMedia("(max-device-width: 768px)").matches){
+    //iPhone
+    appendKikuban(target, col, "iPhone");
+  } else {
+    //web
+  }
+}
+
+function matchHeader(target){
+  //header는 col3 지정
+  target = target.children(".col3");
+
+  if (window.matchMedia("(max-device-width: 1536px)").matches && window.matchMedia("(max-device-width: 768px)").matches == false){
+    //iPad
+  } else if(window.matchMedia("(max-device-width: 768px)").matches){
+    //iPhone
+    target.css("grid-template-columns", iPhoneThreeColumn);
+    imageEdit(target, 2);
+  } else {
+    //web
+  }
+}
